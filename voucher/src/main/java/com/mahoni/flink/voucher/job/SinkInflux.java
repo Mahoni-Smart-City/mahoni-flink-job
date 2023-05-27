376a84d73f0d5d2668168688f7e3548298118250
risw24 <riswanda.alifarahman@gmail.com>
2023-05-27 20:20:26 +0700

    fix voucher job

--- voucher/src/main/java/com/mahoni/flink/voucher/job/SinkInflux.java
+++ voucher/src/main/java/com/mahoni/flink/voucher/job/SinkInflux.java
@@ -5,25 +5,27 @@ import com.influxdb.client.InfluxDBClientFactory;
 import com.influxdb.client.WriteApiBlocking;
 import com.influxdb.client.domain.WritePrecision;
 import com.influxdb.client.write.Point;
-import com.mahoni.flink.voucher.schema.VoucherEnrichment;
+import com.mahoni.schema.VoucherMerchantEnrichment;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 
-public class SinkInflux implements SinkFunction<VoucherEnrichment> {
+public class SinkInflux implements SinkFunction<VoucherMerchantEnrichment> {
 
     @Override
-    public void invoke(VoucherEnrichment voucherEnrichment) throws Exception {
-        /*
-        InfluxDBClient influxDBClient = InfluxDBClientFactory.create("http://localhost:8086", "ovGwppoZfaI6FAWWFl5MBUna0I8vjH0rOc-jGOgH-vS0z2Im3FU2Gq-CoyAWMNp2qmIP8Llrb_uq1E-qGuuGgA==".toCharArray(), "mahoni", "air_quality");
+    public void invoke(VoucherMerchantEnrichment voucherMerchantEnrichment) throws Exception {
+
+        InfluxDBClient influxDBClient = InfluxDBClientFactory.create("http://34.101.97.78:8086", "sFTppz2pO6_iaWRvl0yxcilS5XsREBzwZf0g7eEgyNdKdlsr8Y_0H3-OGnIwpLjZari0WILir5N2EQmiGbd9Zw==".toCharArray(), "mahoni", "voucher-merchant");
 
         WriteApiBlocking writeApi = influxDBClient.getWriteApiBlocking();
 
-        Point point = Point.measurement("air_sensor")
-                .addTag("nameLocation",airQualityProcessedSchema.getNameLocation().toString())
-                .addTag("district",airQualityProcessedSchema.getDistrict().toString())
-                .addField("aqi",airQualityProcessedSchema.getAqi())
-                .time(airQualityProcessedSchema.getTimestamp(), WritePrecision.MS);
+        Point point = Point.measurement("voucher_merchant")
+                .addTag("sex",voucherMerchantEnrichment.getSex().toString())
+                .addTag("voucherName",voucherMerchantEnrichment.getVoucherName().toString())
+                .addTag("typeVoucher",voucherMerchantEnrichment.getVoucherType().toString())
+                .addTag("merchantName",voucherMerchantEnrichment.getMerchantName().toString())
+                .addField("age",voucherMerchantEnrichment.getAge())
+                .addField("voucherId",voucherMerchantEnrichment.getVoucherId().toString())
+                .addField("userId", voucherMerchantEnrichment.getUserId().toString())
+                .time(voucherMerchantEnrichment.getTimestamp(), WritePrecision.MS);
         writeApi.writePoint(point);
-
-         */
     }
 }
--- voucher/src/main/java/com/mahoni/flink/voucher/job/VoucherJob.java
+++ voucher/src/main/java/com/mahoni/flink/voucher/job/VoucherJob.java
@@ -3,8 +3,8 @@ package com.mahoni.flink.voucher.job;
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.Row;
-import com.mahoni.flink.voucher.schema.VoucherEnrichment;
-import com.mahoni.flink.voucher.schema.VoucherRedeemedSchema;
+import com.mahoni.schema.VoucherMerchantEnrichment;
+import com.mahoni.schema.VoucherRedeemedSchema;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
@@ -30,66 +30,85 @@ public class VoucherJob {
         env.setRestartStrategy(RestartStrategies.noRestart());
 
         Properties kafkaConsumerProps = new Properties();
-        kafkaConsumerProps.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        kafkaConsumerProps.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "34.128.127.171:9092");
         kafkaConsumerProps.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         kafkaConsumerProps.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
-        kafkaConsumerProps.setProperty(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+        kafkaConsumerProps.setProperty(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://34.128.127.171:8081");
 
-        DataStream<VoucherRedeemedSchema> voucher = env.addSource(new FlinkKafkaConsumer<>("voucher-redeemed-topic", ConfluentRegistryAvroDeserializationSchema.forSpecific(VoucherRedeemedSchema.class, "http://localhost:8081"), kafkaConsumerProps))
+        DataStream<VoucherRedeemedSchema> voucher = env.addSource(new FlinkKafkaConsumer<>("voucher-redeemed-topic", ConfluentRegistryAvroDeserializationSchema.forSpecific(VoucherRedeemedSchema.class, "http://34.128.127.171:8081"), kafkaConsumerProps))
                 .assignTimestampsAndWatermarks(
                         WatermarkStrategy.forBoundedOutOfOrderness(Duration.ofSeconds(5)));
-        DataStream<VoucherEnrichment> voucherEnrichment = voucher.process(new VoucherProcess());
+        voucher.print();
+        DataStream<VoucherMerchantEnrichment> voucherEnrichment = voucher.process(new VoucherProcess());
+        voucherEnrichment.print();
         voucherEnrichment.addSink(new SinkInflux());
         env.execute("Voucher Job");
     }
-    public static class VoucherProcess extends ProcessFunction<VoucherRedeemedSchema, VoucherEnrichment>{
-        private static final String CASSANDRA_KEYSPACE_1 = "voucher";
-        private static final String CASSANDRA_KEYSPACE_2 = "user";
-        private static final String CASSANDRA_HOST = "localhost";
+
+    public static class VoucherProcess extends ProcessFunction<VoucherRedeemedSchema, VoucherMerchantEnrichment>{
+        private static final String CASSANDRA_KEYSPACE = "mahoni";
+        private static final String CASSANDRA_HOST = "34.101.176.46";
         private static final int CASSANDRA_PORT = 9042;
-        boolean sex;
+        String sex;
+        int sex_decode;
         int age;
         String nameVoucher;
         String typeVoucher;
-        VoucherEnrichment result;
 
-        private transient CqlSession session_1;
-        private transient CqlSession session_2;
+        String merchantId;
+        String nameMerchant;
+        VoucherMerchantEnrichment result;
+
+        private transient CqlSession session;
         @Override
         public void open(Configuration config) {
-            session_1 = CqlSession.builder()
+
+            session = CqlSession.builder()
                     .addContactPoint(InetSocketAddress.createUnresolved(CASSANDRA_HOST, CASSANDRA_PORT))
-                    .withLocalDatacenter("datacenter1")
-                    .withKeyspace(CASSANDRA_KEYSPACE_1)
+                    .withLocalDatacenter("asia-southeast2")
+                    .withKeyspace(CASSANDRA_KEYSPACE)
                     .build();
-            session_2 = CqlSession.builder()
-                    .addContactPoint(InetSocketAddress.createUnresolved(CASSANDRA_HOST, CASSANDRA_PORT))
-                    .withLocalDatacenter("datacenter1")
-                    .withKeyspace(CASSANDRA_KEYSPACE_2)
-                    .build();
+
         }
         @Override
         public void processElement(VoucherRedeemedSchema voucherRedeemedSchema,
                                    Context context,
-                                   Collector<VoucherEnrichment> out) throws Exception {
-            ResultSet userDetail = session_2.execute("SELECT * FROM user WHERE id=" + voucherRedeemedSchema.getUserId());
+                                   Collector<VoucherMerchantEnrichment> out) throws Exception {
+            ResultSet userDetail = session.execute("SELECT * FROM users WHERE id=" + voucherRedeemedSchema.getUserId());
             for(Row rowUser: userDetail){
-                sex = rowUser.getBoolean("sex");
-                age = LocalDate.now().getYear() - rowUser.getInt("year_of_birth");
+                sex_decode = rowUser.getShort("sex");
+                if (sex_decode==1){
+                    sex = "Male";
+                } else if (sex_decode==2){
+                    sex = "Female";
+                } else{
+                    sex = "Unknown";
+                }
+                age = LocalDate.now().getYear() - (int)rowUser.getLong("year_of_birth");
             }
-            ResultSet voucherDetail = session_1.execute("SELECT * FROM voucher WHERE id=" + voucherRedeemedSchema.getVoucherId());
+            ResultSet voucherDetail = session.execute("SELECT * FROM vouchers WHERE id=" + voucherRedeemedSchema.getVoucherId());
             for(Row rowVoucher: voucherDetail){
                 nameVoucher = rowVoucher.getString("name");
                 typeVoucher = rowVoucher.getString("type");
+                merchantId = rowVoucher.getString("merchant_id");
             }
 
-            result = new VoucherEnrichment(voucherRedeemedSchema.getTimestamp(),
+            ResultSet merchantDetail = session.execute("SELECT * FROM merchants WHERE id=" + merchantId);
+            for(Row rowMerchant: merchantDetail){
+                nameMerchant = rowMerchant.getString("name");
+            }
+
+            result = new VoucherMerchantEnrichment(
+                    voucherRedeemedSchema.getEventId(),
+                    voucherRedeemedSchema.getTimestamp(),
                     voucherRedeemedSchema.getVoucherId(),
                     voucherRedeemedSchema.getUserId(),
                     sex,
                     age,
                     nameVoucher,
-                    typeVoucher);
+                    typeVoucher,
+                    nameMerchant
+            );
             out.collect(result);
 
         }
